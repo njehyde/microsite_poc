e1132034bda360883eefc52aa433a8a6ef9ca632
Nicholas Hyde <njehyde@gmail.com>
2022-03-23 22:33:53 +0000

    wrap in async function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const getData = async () => {
-  return await fetch('/.netlify/functions/get-data');
-};
-
-const rootEl = document.getElementById('root');
-const data = await getData();
-const newContent = document.createTextNode(JSON.stringify(data));
-
-rootEl.appendChild(newContent);
-
+(async () => {
+  const getData = async () => {
+    return await fetch('/.netlify/functions/get-data');
+  };
+  
+  const rootEl = document.getElementById('root');
+  const data = await getData();
+  const newContent = document.createTextNode(JSON.stringify(data));
+  
+  rootEl.appendChild(newContent);
+})();
