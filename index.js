201443497859e388947a70233a17a37452a8e1c8
Nicholas Hyde <njehyde@gmail.com>
2022-03-23 22:37:28 +0000

    debugging function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 (async () => {
-  const getData = async () => {
-    return await fetch('/.netlify/functions/get-data');
+  const getData = () => {
+    return fetch('/.netlify/functions/get-data');
   };
   
   const rootEl = document.getElementById('root');
   const data = await getData();
+  const body = data.body;
   const newContent = document.createTextNode(JSON.stringify(data));
   
   rootEl.appendChild(newContent);
